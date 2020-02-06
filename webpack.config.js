321f42c4a4124290d8a0f1e58d424c261524cb8e
Donald Whitely <dswhitely1@gmail.com>
2020-02-06 08:15:05 -0500

    Changed to TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development',
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.ts', '.tsx']
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        use: ['babel-loader']
+        test: /\.tsx?$/,
+        use: ['ts-loader']
       },
       {
         test: /\.html/,
